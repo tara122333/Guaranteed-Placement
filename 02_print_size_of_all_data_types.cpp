68ad1f870b74677488a29440c15a59c18dcbde94
Tara Chand Kumawat <tarachand122333@gmail.com>
2022-05-22 21:36:40 +0530

    print all data types size

---
+++ 02_print_size_of_all_data_types.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+   {
+    int i;
+    char c;
+    float f;
+    double d;
+    long l;
+    short s;
+    string str;
+    bool b;
+    cout<<"size of integer is : "<<sizeof(i)<<" Byte"<<endl;
+    cout<<"size of character is : "<<sizeof(c)<<" Byte"<<endl;
+    cout<<"size of float is : "<<sizeof(f)<<" Byte"<<endl;
+    cout<<"size of double is : "<<sizeof(d)<<" Byte"<<endl;
+    cout<<"size of long is : "<<sizeof(l)<<" Byte"<<endl;
+    cout<<"size of short is : "<<sizeof(s)<<" Byte"<<endl;
+    cout<<"size of string is : "<<sizeof(str)<<" Byte"<<endl;
+    cout<<"size of boolean is : "<<sizeof(b)<<" Byte"<<endl;
+    return 0;
+}
