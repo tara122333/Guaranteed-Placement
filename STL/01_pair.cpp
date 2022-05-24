974502708a7218b60060bd40341dca939b5904f8
Tara Chand Kumawat <tarachand122333@gmail.com>
2022-05-24 22:06:40 +0530

    pair in STL

---
+++ STL/01_pair.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+   {
+    pair<int,int>p;
+    p = make_pair(45,63);
+    pair<int,int>p1;
+    p1.first = 55;
+    p1.second = 123;
+
+    cout<<p.first<<" "<<p.second<<endl;
+    cout<<p1.first<<" "<<p1.second<<endl;
+    return 0;
+}
