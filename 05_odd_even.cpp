83cfbecb8458f332d35904120d3a5c67acc8a78a
Tara Chand Kumawat <tarachand122333@gmail.com>
2022-05-26 23:35:37 +0530

    find number is odd or even

---
+++ 05_odd_even.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+   {
+    int n;
+    cin>>n;
+    if(n%2==0){
+        cout<<n<<" is Even Number "<<endl;
+    }
+    else{
+        cout<<n<<" is Odd Number "<<endl;
+    }
+    return 0;
+}
